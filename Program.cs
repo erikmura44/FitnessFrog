f9a01b46902444221c019122f363b2851d4dac09
erikmura44 <erikmura44@gmail.com>
2017-05-08 13:08:14 -0600

    console

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ class Program
   static void Main()
   {
     // Prompt the user for minutes exercised
+    System.Console.Write("Enter how many minutes you exercised: ");
     // Add minutes exercised to total
     // Display total minnutes exercised to the screen
     // Repeat until the user quits the App
