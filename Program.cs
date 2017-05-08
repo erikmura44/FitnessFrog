a84f36c2b608e9119446a410770ff4a8dffcc26b
erikmura44 <erikmura44@gmail.com>
2017-05-08 14:13:55 -0600

    basic working console app

--- Program.cs
+++ Program.cs
@@ -4,21 +4,24 @@ namespace Treehouse.FitnessFrog
 {
   class Program
   {
-    static void Main()
-    {
+    static void Main(){
+
       int runningTotal = 0;
+      bool keepGoing = true;
 
-      // Prompt the user for minutes exercised
-      Console.Write("Enter how many minutes you exercised: ");
-      string entry = Console.ReadLine();
+      while (keepGoing){
+        Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+        string entry = Console.ReadLine();
 
-      int minutes = int.Parse(entry);
-      runningTotal = runningTotal + minutes;
-
-      // Add minutes exercised to total
-      // Display total minnutes exercised to the screen
-      Console.WriteLine("You've exercised " + runningTotal +  " minutres");
-      // Repeat until the user quits the App
+        if (entry == "quit"){
+          keepGoing = false;
+        } else {
+          int minutes = int.Parse(entry);
+          runningTotal = runningTotal + minutes;
+          Console.WriteLine("You've exercised " + runningTotal +  " minutres");
+        }
+      }
+      Console.WriteLine("Goodbye");
     }
   }
 }
